4fe7317775013ec441fd64ada701d1cddef96b04
yoichi-nasuno <nyoichi223@gmail.com>
2021-01-12 02:15:06 +0000

    git commit again

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,39 @@
 class BooksController < ApplicationController
   def create
     # @book = book.find_by(book_name: :param[:book_author])
+    
+    # book の保存
     @book = Book.new
+    # params == { "book" => { "book_name" => "xxx", "author_name" => "yyyy" } }
     @book.book_name = params[:book][:book_name]
     if @book.save
       flash[:success] = '本が登録されました'
     else
-      flaxh[:danger] = '本が登録されませんでした'
+      flash[:danger] = '本が登録されませんでした'
     end 
+
+    # author の保存
     @author = Author.new
     @author.author_name = params[:book][:author_name]
-    
     if @author.save
       flash[:success] = '著者が登録されました'
     else
       flash[:danger] = '著者が登録されませんでした'
     end 
     
-    @book_author = @book.authors.build
-    if @book_author.save
-      flash[:success] = '登録されました'
-      redirect_to @book
-    else
-      flash.now[:danger] = '登録されませんでした'
-      render :index
-    end 
+    # 中間テーブルの保存
+    # book_authorsテーブル: id, book_id, author_id
+    @book_author = @book.book_authors.find_or_create_by(author_id: @author.id)
+    redirect_to @book
+    # if @book_author.save
+    #   flash[:success] = '登録されました'
+    #   redirect_to @book
+    # else
+    #   flash.now[:danger] = '登録されませんでした'
+    #   @books = Book.all
+    #   @authors = Author.all
+    #   render :index
+    # end 
   end
   
   def new
